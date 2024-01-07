0153144ae01e1b1120860ab39210d5effb3762ae
Victor Gomes <victorgomescorrea19@gmail.com>
2024-01-07 17:23:13 -0300

    post request added in SpringClient

--- src/main/java/com/devdojo/springboot/client/SpringClient.java
+++ src/main/java/com/devdojo/springboot/client/SpringClient.java
@@ -3,8 +3,7 @@ package com.devdojo.springboot.client;
 import com.devdojo.springboot.domain.Animes;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -14,19 +13,33 @@ import java.util.List;
 public class SpringClient {
     public static void main(String[] args) {
         ResponseEntity<Animes> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/1", Animes.class);
-        log.info(entity);
+        log.info(entity+"\n");
 
         Animes entity1 = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Animes.class, 2);
-        log.info(entity1); // Returns the object without the information that we have using getForEntity
+        log.info(entity1+"\n"); // Returns the object without the information that we have using getForEntity
 
         Animes[] entity2 = new RestTemplate().getForObject("http://localhost:8080/animes/all", Animes[].class);
-        log.info(Arrays.toString(entity2)); // Returns an Array of objects
+        log.info(Arrays.toString(entity2)+"\n"); // Returns an Array of objects
 
         ResponseEntity<List<Animes>> animesList = new RestTemplate().exchange("http://localhost:8080/animes/all",
                 HttpMethod.GET, null, new ParameterizedTypeReference<>() {
                 });
-        log.info(animesList.getBody()); // Returns a list of objects
+        log.info(animesList.getBody()+"\n"); // Returns a list of objects
 
+//        Animes anime = Animes.builder().name("Kingdom").build();
+//        Animes kingdom = new RestTemplate().postForObject("http://localhost:8080/animes", anime, Animes.class);
+//        log.info(kingdom+"\n");
 
+        Animes anime = Animes.builder().name("Sakurasou pet no kanojo").build();
+        ResponseEntity<Animes> animeSaved = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST, new HttpEntity<>(anime, createJsonHeader()), Animes.class);
+
+        log.info(animeSaved+"\n");
+
+    }
+    private static HttpHeaders createJsonHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }
